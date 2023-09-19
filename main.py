eabee74d649cc4bbe3fef58316768e812fa65fff
Markul13 <145430809+Markul13@users.noreply.github.com>
2023-09-19 23:39:03 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 a = int(input())
-if a%4==0 and a%100!=0 or a%400==0:
-    print('YES')
-else:
-    print('NO')
-
-
+b = int(input())
+c = int(input())
+d = (b**2) - (4*a*c)
+if d>0:
+    x1 = (-b-d**0.5)/(2*a)
+    x2 = (-b+d**0.5)/(2*a)
+    print(x1,x2)
+if d == 0:
+    x = (-b)/(2*a)
+    print(x)
+if d<0:
+    print('Нет корней')
